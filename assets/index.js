64d39c9e04abb99d5e4bd4b6ffd7f51936bc7ccc
jackcrane <jack-mac@users.noreply.github.com>
2021-09-20 20:04:26 -0400

    first commit

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('scroll', (e) => {
+  if(window.scrollY > 225) {
+    document.querySelector('.site-header').classList.add('scrolled')
+  } else {
+    document.querySelector('.site-header').classList.remove('scrolled')
+  }
+})
